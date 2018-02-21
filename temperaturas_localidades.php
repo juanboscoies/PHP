7f63d3b36302ed543eb48cb3764fd5f0ca30a863
juanboscoies <33419744+juanboscoies@users.noreply.github.com>
2018-02-21 22:13:50 +0100

    Update temperaturas_localidades.php

--- temperaturas_localidades.php
+++ temperaturas_localidades.php
@@ -13,16 +13,15 @@ $contador=0;
 $n = 20;
 
 foreach($temp as $localidad => $valor) {
+	print "La temperatura media en " . $localidad . " es " . $valor."<br/>";
+	
+	$acumulador += $valor; //$acumulador = $acumulador + $valor
 
-print "La temperatura media en " . $localidad . " es " . $valor."<br/>";
-
-$acumulador += $valor; //$acumulador = $acumulador + $valor
-
-if( $valor  > $n  )
-	$contador++;
+	if( $valor > $n  )
+		$contador++;
 }
 
-$temp_media = $acumulador / count($temp); //->Generar error de división por cero o no definición de variable
+$temp_media = $acumulador / count($temp);
 
 print "<br>Temperatura media del a&ntilde;o: " . $temp_media;
 print "<br>Número de localidad con temperatura mayor a $n grados: " . $contador;
@@ -30,7 +29,6 @@ print "<br>Número de localidad con temperatura mayor a $n grados: " . $contador
 unset($temp);
 
 //Propuesta: Mostrar el número de localidades con temperatura mayor a una dada.
-
 //echo "<br><pre>" . print_r($temp) . "</pre>";
 //var_dump($temp);
 
