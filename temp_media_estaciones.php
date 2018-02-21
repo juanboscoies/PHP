18abec8614fe3930664dddf77c8d6aff8732a36d
juanboscoies <33419744+juanboscoies@users.noreply.github.com>
2018-02-21 20:50:09 +0100

    Update temp_media_estaciones.php

--- temp_media_estaciones.php
+++ temp_media_estaciones.php
@@ -1,20 +1,15 @@
 <?php
 
 $temperatura["Primavera"] = 21;
-
 $temperatura["Verano"] = 37;
-
 $temperatura["Otonyo"] = 12;
-
 $temperatura["Invierno"] = 7;
 
 $acumulador = 0;
 
 foreach($temperatura as $estacion => $valor) {
-
-print "La temperatura media en " . $estacion . " es " . $valor."<br/>";
-$acumulador += $valor;
-
+  print "La temperatura media en " . $estacion . " es " . $valor."<br/>";
+  $acumulador += $valor;
 }
 
 $temp_media= $acumulador / count($temperatura);
