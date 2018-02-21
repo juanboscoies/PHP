8a5e09e2c2d70d19c6577532e02141244e8f23af
juanboscoies <33419744+juanboscoies@users.noreply.github.com>
2018-02-21 22:05:23 +0100

    Update ficheros_ejB_contadorDoble.php

--- ficheros_ejB_contadorDoble.php
+++ ficheros_ejB_contadorDoble.php
@@ -6,10 +6,6 @@ del valor de $contador, es decir, que muestre 2, 4, 6, 8, 10.
 El resultado se guardará en un fichero de nombre "contador_doble.txt".
 */
 
-print "Crea un bucle for que a partir de una variable \$contador que toma valores de 1 a 5, 
-		muestre por pantalla el doble del valor de \$contador, es decir, que muestre 2, 4, 6, 8, 10.<br>
-		Guarda el resultado en un fichero de nombre \"contador_doble.txt\"<br><br>";
-
 $f = fopen("contador_doble.txt", "w");
 fputs($f, "RESULTADO EXPORTADO AL FICHERO:\n\n\n");	
 		
