c47fd2846a3924395b7d3d47bfa0ea0cdd83492e
juanboscoies <33419744+juanboscoies@users.noreply.github.com>
2018-02-21 22:09:54 +0100

    Update for_arrays_multidimensionales.php

--- for_arrays_multidimensionales.php
+++ for_arrays_multidimensionales.php
@@ -1,11 +1,8 @@
 <?php
 
 $array[0][0] = "Uno";
-
 $array[0][1] = "Dos";
-
 $array[1][0] = "Tres";
-
 $array[1][1] = "Cuatro";
 
 for($i=0; $i<count($array); $i++)
@@ -20,13 +17,9 @@ for($i=0; $i<count($array); $i++)
 echo "<br />--------------------<br /><br />";
 
 $array2[0][0][0] = "Cinco";
-
 $array2[0][0][1] = "Seis";
-
 $array2[0][0][2] = "Siete";
-
 $array2[0][1][0] = "Ocho";
-
 $array2[0][1][1] = "Nueve";
 
 for($i=0; $i<count($array2); $i++)
